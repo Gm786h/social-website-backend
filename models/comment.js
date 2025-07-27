d6083e3d85526036e37add18dfde5887d32c55ef
Gm786h <149281934+Gm786h@users.noreply.github.com>
2025-07-28 03:46:31 +0500

    Update comment.js

--- models/comment.js
+++ models/comment.js
@@ -1,18 +1,26 @@
-
-
+// Comment Model
 'use strict';
-
 module.exports = (sequelize, DataTypes) => {
   const Comment = sequelize.define('Comment', {
     content: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT, // Changed from STRING to TEXT for longer comments
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    postId: {
+      type: DataTypes.INTEGER,
       allowNull: false
     }
+  }, {
+    tableName: 'comments', // Added lowercase table name
+    timestamps: true
   });
 
   Comment.associate = (models) => {
     Comment.belongsTo(models.User, { foreignKey: 'userId' });
-
     Comment.belongsTo(models.Post, { foreignKey: 'postId' });
   };
 
