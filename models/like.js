6a614e171344807f6b41eb3f1424363ad4da920a
Gm786h <149281934+Gm786h@users.noreply.github.com>
2025-07-28 03:40:19 +0500

    Update like.js

--- models/like.js
+++ models/like.js
@@ -1,14 +1,30 @@
 'use strict';
-
 module.exports = (sequelize, DataTypes) => {
-  const Like = sequelize.define('Like', {}, { timestamps: false });
+  const Like = sequelize.define('Like', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    tableName: 'Likes',
+    timestamps: true,
+    updatedAt: false, // Only track createdAt for likes
+    indexes: [
+      {
+        unique: true,
+        fields: ['userId', 'postId'] // Prevent duplicate likes
+      }
+    ]
+  });
 
   Like.associate = (models) => {
-    
     Like.belongsTo(models.User, { foreignKey: 'userId' });
     Like.belongsTo(models.Post, { foreignKey: 'postId' });
   };
 
   return Like;
 };
-
