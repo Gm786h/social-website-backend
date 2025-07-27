33c395a645bd9bf3d2757a9f7486dc42ed494a28
Gm786h <149281934+Gm786h@users.noreply.github.com>
2025-07-28 03:34:17 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 'use strict';
-
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
@@ -15,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-      profileImageUrl: {
+    profileImageUrl: {
       type: DataTypes.STRING,
       allowNull: true
     },
@@ -23,34 +22,35 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: true
     }
+  }, {
+    // Add this options object to specify the correct table name
+    tableName: 'users', // This tells Sequelize to use the 'users' table (lowercase)
+    timestamps: true,   // Assumes you have createdAt/updatedAt columns
+    underscored: false  // Keeps camelCase column names
   });
+
   User.associate = (models) => {
     User.hasMany(models.FriendRequest, {
       as: 'SentRequests',
       foreignKey: 'senderId'
     });
-
     User.hasMany(models.FriendRequest, {
       as: 'ReceivedRequests',
       foreignKey: 'receiverId'  
     });
-
     User.hasMany(models.Post, { foreignKey: 'userId' });
     User.hasMany(models.Comment, { foreignKey: 'userId' });
     User.hasMany(models.Like, { foreignKey: 'userId' });
   
     User.hasMany(models.Message, {
-    as: 'SentMessages',
-    foreignKey: 'senderId'
-  });
-
-  User.hasMany(models.Message, {
-    as: 'ReceivedMessages',
-    foreignKey: 'receiverId'
-  });
-
-  
+      as: 'SentMessages',
+      foreignKey: 'senderId'
+    });
+    User.hasMany(models.Message, {
+      as: 'ReceivedMessages',
+      foreignKey: 'receiverId'
+    });
   };
-
+  
   return User;
 };
