570bdba004a94fe65339e1ee8cf946b54ad6e1e7
ghulam <ghulamaham786@gmail.com>
2025-07-28 01:29:57 +0500

    data base changed

--- config/db.js
+++ config/db.js
@@ -1,21 +1,26 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sqdbs', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log(' Connection has been established successfully.');
+    console.log('Connection has been established successfully.');
   } catch (error) {
-    console.error(' Unable to connect to the database:', error);
+    console.error('Unable to connect to the database:', error);
   }
 }
 
 testConnection();
 
 module.exports = { sequelize, Sequelize };
-
